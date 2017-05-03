6fee7954a4b58dc3a62c6d7779a95983afabfeed
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-03 16:54:22 -0700

    setting up reusable components

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { View , Text } from 'react-native';
+import Slides from '../components/Slides';
 
 class WelcomeScreen extends Component {
   render () {
     return (
       <View>
-        <Text> WelcomeScreen</Text>
-        
+        <Slides />
       </View>
     )
   }
