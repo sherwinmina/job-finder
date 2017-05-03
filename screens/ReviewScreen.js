6fce4ddcaea45d55af5c8c692f6f4dab2b78519a
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-03 14:48:00 -0700

    initial commit

---
+++ screens/ReviewScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View , Text } from 'react-native';
+
+class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: 'Review Jobs'
+  }
+
+  render () {
+    return (
+      <View>
+        <Text> ReviewScreen</Text>
+        
+      </View>
+    )
+  }
+}
+
+export default ReviewScreen;
