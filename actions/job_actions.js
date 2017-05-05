9ef144cc6e7a9da74760ec998314a569083d68c0
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-05 14:03:55 -0700

    finisihed url builder for requesting data

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -15,10 +15,18 @@ const JOB_QUERY_PARAMS = {
   q: 'javascript'
 };
 
+const buildJobsUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+}
+
 
 export const fetchJobs = (region) =>  async dispatch => {
   try {
     let zip = await reverseGeocode(region);
+    const url = buildJobsUrl(zip);
+    let data = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data });
   } catch(e) {
     console.error(e);
   }
