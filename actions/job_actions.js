ce96e89c99f48dbc322a169d0f917aca6b9f8555
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-05 13:51:54 -0700

    implemented the indeed api

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
 
 import {
   FETCH_JOBS
 } from './types';
+
+const JOB_QUERY_PARAMS = {
+  publisher: '749373896210754',
+  format: 'json',
+  v: '2',
+  latlong: 1
+};
+
 
 export const fetchJobs = (region) =>  async dispatch => {
   try {
