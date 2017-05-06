5436158fe59ef4c13cd14c1ce1b76d310c563ef7
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-06 09:08:45 -0700

    finishing deck screen

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,20 +1,71 @@
 import React, { Component } from 'react';
-import { View , Text } from 'react-native';
+import { View , Text, Platform } from 'react-native';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
+import Swipe from  '../components/Swipe';
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    const initialRegion = {
+      longtitutde: job.longtitude,
+      latitude: job.latitude,
+      latitudeDelta: 0.045,
+      longtitudeDelta: 0.02
+    };
+
+    return (
+      <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView scrollEnabled={false}
+          style={{ flex: 1 }}
+          cacheEnabled={ Platform.Os === 'android' ? true : false }
+          initialRegion={initialRegion}
+          >
+          </MapView>
+        </View>
+    
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}
+        </Text>
+      </Card>
+    )
+  }
+
+  renderNoMoreCards() {
+    return ( 
+      <Card title= "No more jobs">
+      </Card>
+    )
+  }
+
   render () {
     return (
       <View>
-        <Text>DeckScreen</Text>
-        
+        <Swipe
+          data={this.props.jobs}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCard}
+         />
       </View>
     )
   }
 }
 
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
+}
+
 function mapStateToProps({ jobs }) {
- return { jobs: jons.results };
+ return { jobs: jobs.results };
 }
 
 export default connect(mapStateToProps)(DeckScreen);
