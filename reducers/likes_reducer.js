5b110bd6d8fd298dae64e2b4512d9db77f285698
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-07 10:49:00 -0700

    updated review screen

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -4,14 +4,13 @@ import {
   LIKE_JOB
 } from '../actions/types';
 
-export default function(state, action) {
+export default function(state = [], action) {
   switch (action.type) {
     case LIKE_JOB:
       return _.uniqBy([
         action.payload, ...state
       ], 'jobkey');
     default: 
-      return state;
-    
+      return state; 
   }
 }
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View , Text, Platform } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View , Text, Platform, ScrollView } from 'react-native';
+import { Button, Card } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 class ReviewScreen extends Component {
   // static navigationOptions = {
@@ -17,13 +18,45 @@ class ReviewScreen extends Component {
   //   }
   // }
 
+  renderLikedJobs() {
+    return this.props.likedJobs.map(job => {
+      return (
+        <Card>
+          <View style={{ height: 200 }}>
+            <View style={styles.detailWrapper}>
+              <Text style={styles.italics}>{job.company}</Text>
+              <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
+            </View>
+          
+          </View>
+        </Card>
+      );
+    });
+  }
+
   render () {
     return (
-      <View>
-        <Text> ReviewScreen</Text>  
-      </View>
+      <ScrollView>
+         {this.renderLikedJobs()}
+      </ScrollView>
     )
   }
 }
 
-export default ReviewScreen;
+const styles = {
+  italics: {
+    fontStyle: 'italic'
+  },
+  detailWrapper: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+}
+
+function mapStateToProps(state) {
+  return { likedJobs: state.likedJobs };
+}
+
+export default connect(mapStateToProps)(ReviewScreen);
+
