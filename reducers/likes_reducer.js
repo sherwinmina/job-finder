bda2617996b90743074bf8ff27e4c3f1c84bbedf
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-07 10:04:03 -0700

    added like job action and reducer

---
+++ reducers/likes_reducer.js
@@ -0,0 +1,13 @@
+import {
+  LIKE_JOB
+} from '../actions/types';
+
+export default function(state, action) {
+  switch (action.type) {
+    case LIKE_JOB:
+      return 
+    default: 
+      return state;
+    
+  }
+}
