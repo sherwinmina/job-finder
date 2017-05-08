1d2ee37650641dcd7d1cc5f249dabf2f7a99c2b7
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-08 11:15:59 -0700

    added push notification

---
+++ services/push_notification.js
@@ -0,0 +1,18 @@
+import { Permissions, Notifications } from 'expo';
+
+export default async () => {
+  let previousToken = await AsyncStorage.getItem('pushtoken');
+  
+  if (previous) {
+    return;
+  } else {
+    let { status } = await Permissions.askAsync(Permissions.REMOTE_NOTIFICATIONS);
+
+    if (status !== granted) {
+      return;
+    }
+    
+    let token = await Notifications.getExponentPushTokenAsync();
+
+  }
+};
